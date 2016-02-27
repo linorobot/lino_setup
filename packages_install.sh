39341db5a416a503311564e1b7a307ba42b19ca3
Juan Jimeno <jimenojmm@gmail.com>
2016-02-28 01:11:49 +0800

    added packages installer

---
+++ packages_install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+cd ~/catkin_ws/src
+
+git clone https://github.com/grassjelly/lino_pid.git
+git clone https://github.com/grassjelly/linorobot.git
+git clone https://github.com/rohbotics/xv_11_laser_driver.git
+git clone https://github.com/tonybaltovski/ros_arduino.git
+
