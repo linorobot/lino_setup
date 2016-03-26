4152dd2c6e7941f7b6effc186efabdf7a473680d
Juan Jimeno <jimenojmm@gmail.com>
2016-03-26 20:33:00 +0800

    edit packages

--- ros_packages_install.sh
+++ ros_packages_install.sh
@@ -1,12 +1,12 @@
 sudo apt-get update
-sudo apt-get install ros-indigo-navigation
 sudo apt-get install ros-indigo-roslint
 sudo apt-get install ros-indigo-rosserial
-sudo apt-get install ros-indigi-imu-filter-madgwick
+sudo apt-get install ros-indigo-imu-filter-madgwick
+sudo apt-get install ros-indigo-xv-11-laser-driver
+sudo apt-get install ros-indigo-navigation
 cd ~/catkin_ws/src
 
 git clone https://github.com/grassjelly/lino_pid.git
 git clone https://github.com/grassjelly/linorobot.git
-git clone https://github.com/rohbotics/xv_11_laser_driver.git
 git clone https://github.com/tonybaltovski/ros_arduino.git
 
