1523cfde129ce2e911c4d04e34844e840cc2c57b
Juan Miguel Jimeno <jimenojmm@gmail.com>
2016-04-04 23:45:19 +0800

    edit python-pip to pip

--- dev_tools_install.sh
+++ dev_tools_install.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get install avahi-daemon
 sudo apt-get install openssh-server
 sudo apt-get install arduino-core
-sudo easy_install python-pip
+sudo easy_install pip
 sudo pip install -U platformio
 
 #for raspberry pi
