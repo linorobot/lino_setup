6811e1a054d440345d0830fe869f68dc2d9bed75
Juan Miguel Jimeno <jimenojmm@gmail.com>
2016-04-06 12:00:24 +0800

    added map_server packge

--- ros_packages_install.sh
+++ ros_packages_install.sh
@@ -3,7 +3,9 @@ sudo apt-get install ros-indigo-roslint
 sudo apt-get install ros-indigo-rosserial
 sudo apt-get install ros-indigo-imu-filter-madgwick
 sudo apt-get install ros-indigo-xv-11-laser-driver
+sudo apt-get install ros-indigo-gmapping
 sudo apt-get install ros-indigo-navigation
+sudo apt-get install ros-indigo-map-server
 cd ~/catkin_ws/src
 
 git clone https://github.com/grassjelly/lino_pid.git
